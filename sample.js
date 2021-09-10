e1e260cf486f1985fc6d87f0c7505fc020600f8f
ninja03 <69106571+ninja03@users.noreply.github.com>
2021-09-10 20:53:25 +0900

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,20 @@
+import { Algorithm } from "https://raw.githubusercontent.com/kakomimasu/client-deno/v1.0.0-beta.0/algorithm.js";
+
+export class Sample extends Algorithm {
+  onInit(boardPoints, agentNum, turnNum) {
+  }
+
+  onTurn(field, pid, agents, turn) {
+    return [];
+  }
+}
+
+if (import.meta.main) {
+  const a = new Sample();
+  a.match({
+    id: "sample",
+    name: "sample",
+    spec: "サンプル",
+    password: "sample-pw",
+  });
+}
